f93615be5c4733cb1f2e295f0c454b742ba5ba99
ibyb007 <58368196+ibyb007@users.noreply.github.com>
2026-09-10 08:46:34 +0530

    Add audio preamp plugin to app configuration
    Added audio preamp plugin to the configuration.

--- app.config.js
+++ app.config.js
@@ -13,6 +13,7 @@ module.exports = () => {
   const plugins = [
     './plugins/with-android-tv.js',
     './plugins/withKeyEvent.js',
+    './plugins/with-audio-preamp.js',
     './plugins/with-custom-native-modules.js',
     './plugins/android-native-config.js',
     './plugins/with-saf-copy-module.js',
